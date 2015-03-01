7532a51937497b366aaf7edbe57dfa0984703559
Amjad Hanini <amjad16@meet.mit.edu>
2015-03-01 20:03:35 +0200

    hi

---
+++ project1/p.py
@@ -0,0 +1,14 @@
+class Player:
+	def_init_(self,name,score,level,money):
+		self.name=name
+		self.score=score
+		self.level=level
+		self.money=money
+	def print_all(self):
+		print(self.name)
+		print(self.score)
+		print(self.level)
+		print(self.money)
+
+
+
