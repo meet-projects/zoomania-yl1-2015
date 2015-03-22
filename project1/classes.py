ccffd5d2415dc70834fbe7563c8db5780ac94648
Berge Hagopian <berge16@meet.mit.edu>
2015-03-22 19:57:16 +0200

    everything

--- project1/classes.py
+++ project1/classes.py
@@ -1,5 +1,5 @@
 class Player:
-	def_init_(self,name,score,level,money):
+	def_init_(self,name,score,level,money)
 		self.name=name
 		self.score=score
 		self.level=level
@@ -9,6 +9,79 @@ class Player:
 		print(self.score)
 		print(self.level)
 		print(self.money)
+	def Lose_Life(self):
+		if life = 0
+			print "You Lose"
 
+
+#Path:
+
+turtle.penup()
+turtle.hideturtle()
+turtle.speed(0)
+turtle.goto(-925,400)
+turtle.pendown()
+turtle.goto(-550,400)
+turtle.goto(-550,-200)
+turtle.goto(350,-200)
+turtle.goto(350,200)
+turtle.goto(1000,200)
+turtle.penup()
+turtle.goto(-925,325)
+turtle.pendown()
+turtle.goto(-625,325)
+turtle.goto(-625,-275)
+turtle.goto(425,-275)
+turtle.goto(425,125)
+turtle.goto(1000,125)
+turtle.hideturtle()
+turtle.mainloop()
+		
+class Animal(turtle):
+	def__init__(self, x, y, Type, speed, health)
+		self.penup()
+		self.goto(x,y)
+		self.x= x
+		self.y= y
+		self.Type= Type
+		self.speed= speed
+		self.health= health
+
+	def Disappearing (self):
+		if health = 0
+			turtle.hideturtle()
+
+	def Loser_Disappearing (self):
+			if x = 100 y = 100
+				turtle.hideturtle()
+
+
+class Machine(turtle):
+	def __init__(self,x,y,shape,animal,shoot)
+		self.goto(x,y)
+		self.shoot=shoot
+		self.animal=animal
+		self.shape(shape)
+
+class bullets(turtle):
+	def __init__(self,x,y,type,shape)
+		self.goto(x,y)
+		self.type=type
+		self.shape(shape)
+	def getRadius(self):
+	    return 2
+
+class Store (turtle):
+	def__init__(self,x,y)
+		self.goto(x,y)
+
+	turtle.penup():
+	turtle.hideturtle()
+	turtle.speed(0)
+	turtle.goto(2000,600)
+	turtle.goto(2250,600)
+	turtle.goto(2250,800)
+	turtle.goto(2000,800)
+	turtle.penup():
 
 
